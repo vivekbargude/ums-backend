2e46761c01b853facaa0abfff75a5b1288b30f6e
vivekbargude <vivekbargude1206@gmail.com>
2024-08-29 22:32:13 +0530

    inventory route

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const app = require('./app/app');
 const PORT = 3000
 
 
-
-
 //SERVER CONNECTION
 app.listen(PORT,()=>{
     console.log(`Server is listening at http://localhost:${PORT}`.blue.underline.bold);
